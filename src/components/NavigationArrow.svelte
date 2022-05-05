b826889c14bfdf494ad305393e15c68eb81999f7
Lesley Huang <ningning621@gmail.com>
2022-05-05 09:37:26 -0700

    add nav arrow

---
+++ src/components/NavigationArrow.svelte
@@ -0,0 +1,17 @@
+<script>
+  export let link;
+  // TODO: move scrolling logic to this class
+</script>
+
+<a href={link}>
+  <img src="/images/chevron.png" alt="arrow">
+</a>
+
+<style>
+  img {
+    width: 50px;
+    height: 50px;
+    position: fixed;
+    bottom: 5%;
+  }
+</style>
--- src/components/TitlePage.svelte
+++ src/components/TitlePage.svelte
@@ -1,6 +1,11 @@
-<div>
+<script>
+  import NavigationArrow from "./NavigationArrow.svelte";
+</script>
+
+<div id="page_1">
   <h1>Hell and Heaven: Who goes and what happens?</h1>
   <h3>A look into what 6000+ U.S. adults think about the afterlife.</h3>
+  <NavigationArrow link="#page_2"/>
 </div>
 
 <style>
