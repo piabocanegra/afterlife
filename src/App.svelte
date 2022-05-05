6e7579387f1e3b423f8208fe039fdc78214b5fcb
Lesley Huang <ningning621@gmail.com>
2022-05-05 00:03:05 -0700

    initial svelte set up

---
+++ src/App.svelte
@@ -0,0 +1,30 @@
+<script>
+	import Scroller from '@sveltejs/svelte-scroller';
+	import TitlePage from './TitlePage.svelte';
+	let top = 0.1;
+	let threshold = 0.5;
+	let bottom = 0.9;
+</script>
+
+
+<main>
+			<TitlePage/>
+			<TitlePage/>
+</main>
+
+
+<style>
+	:global(h1, h3) {
+		padding: 1em;
+    max-width: 50%;
+    margin: auto;
+		font-family: minion-pro-caption, serif;
+		font-weight: 400;
+		font-style: normal;
+	}
+	main {
+		text-align: center;
+		max-width: 1000px;
+		margin: 0 auto;
+	}
+</style>
---
+++ src/TitlePage.svelte
@@ -0,0 +1,13 @@
+<div>
+  <h1>Hell and Heaven: Who goes and what happens?</h1>
+  <h3>A look into what 6000+ U.S. adults think about the afterlife.</h3>
+</div>
+<style>
+  div {
+    text-align: center;
+
+    background-color: #cdcdcd;
+    width: 100%;
+    min-height:100vh;
+  }
+</style>
