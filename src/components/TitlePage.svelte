ed58826e64ad9726cc2ddbb3eaa4cc5011ddc907
Sofia Bocanegra <sib35@cornell.edu>
2022-05-05 19:56:48 -0700

    Update TitlePage.svelte

--- src/components/TitlePage.svelte
+++ src/components/TitlePage.svelte
@@ -2,8 +2,16 @@
   import NavigationArrow from "./NavigationArrow.svelte";
 </script>
 
+<<<<<<< Updated upstream
+=======
+<<<<<<< HEAD
+<div id="page_1">
+  <h1 class="ConcordeRegular">Hell and Heaven: Who goes and what happens?</h1>
+=======
+>>>>>>> Stashed changes
 <div class="page" id="page_1">
   <h1>Hell and Heaven: Who goes and what happens?</h1>
+>>>>>>> 5550df359b695e58affb74e02f9cd672f27dcce3
   <h5>A look into what 6,000+ U.S. adults think about the afterlife</h5>
   <div id="image_wrapper">
     <img src="/images/angels.png" alt="angels">
@@ -13,11 +21,20 @@
 
 <style>
   div {
+<<<<<<< Updated upstream
+=======
+<<<<<<< HEAD
+    font-family: ConcordeRegular
+    text-align: center;
+=======
+>>>>>>> 5550df359b695e58affb74e02f9cd672f27dcce3
+>>>>>>> Stashed changes
     position: relative;
     background-color: #DDC3B1;
     @font-face {
       font-family: "TitleFont";
     }
+<<<<<<< Updated upstream
   }
   div#image_wrapper {
     width: 100%;
@@ -28,5 +45,28 @@
   }
   img {
     text-align: center;
+=======
+  }
+<<<<<<< HEAD
+
+  @font-face {
+  font-family: "ConcordeRegular";
+  src: url("/fonts/ConcordeRegular.ttf") format('ttf');
+  }
+
+  .ConcordeRegular {
+  font-family: "ConcordeRegular";
+=======
+  div#image_wrapper {
+    width: 100%;
+    position: absolute;
+    bottom: 0%;
+    margin-left: auto;
+    margin-right: auto;
+  }
+  img {
+    text-align: center;
+>>>>>>> 5550df359b695e58affb74e02f9cd672f27dcce3
+>>>>>>> Stashed changes
   }
 </style>
