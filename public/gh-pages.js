fec482f2ac63ef41b7a4153f752485eaa287cbfa
Lesley Huang <ningning621@gmail.com>
2022-05-20 17:01:23 -0700

    deploy 2

--- public/gh-pages.js
+++ public/gh-pages.js
@@ -3,7 +3,7 @@ var ghpages = require('gh-pages');
 ghpages.publish(
     'public', // path to public directory
     {
-        branch: 'gh-pages',
+        branch: 'release',
         repo: 'https://github.com/piabocanegra/afterlife.git', 
         user: {
             name: 'Pia Bocanegra and Lesley Huang', 
