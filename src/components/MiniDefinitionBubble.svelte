f7ac70325577094b55b47318211a2b8769b653da
Lesley Huang <ningning621@gmail.com>
2022-05-31 23:36:19 -0700

    finished religiously unaffiliated vis

---
+++ src/components/MiniDefinitionBubble.svelte
@@ -0,0 +1,54 @@
+<script>
+  export let position;
+  export let text;
+  export let tooltipOffset;
+
+  // variables for image bubble
+  let x = position[0];
+  let y = position[1];
+
+  // variables for tooltip text
+  let tooltipText = text;
+  let tooltipTop = 0;
+  let tooltipLeft = 0;
+  let tooltipTranslate = "";
+  let tooltipVisibility = false;
+
+  function handleMouseOver() {
+    tooltipVisibility = true;
+  }
+
+  function handleMouseOut() {
+    tooltipVisibility = false;
+  }
+
+ 
+</script>
+
+<!-- svelte-ignore a11y-mouse-events-have-key-events -->
+<image x={x} y={y} width=20 height=20 on:mouseover={handleMouseOver} on:mouseout={handleMouseOut} href="/images/definition.svg" />
+
+{#if tooltipVisibility}
+  <rect x={x-125} y={y+30} width=190 height={tooltipText.length*20} rx=5 ry=5/>
+  {#each tooltipText as txt, i}
+    <text x={x-30} y={y+50+i*15}>{txt}</text>
+  {/each}
+{/if}
+
+
+<style>
+  image {
+    cursor: pointer;
+  }
+
+  text {
+    font-size: 14px;
+    text-anchor: middle;
+  }
+
+  rect {
+    stroke-width: 1px;
+    stroke: black;
+    fill: none;
+  }
+</style>
--- src/screens/ReligiouslyUnaffiliatedLineVis.svelte
+++ src/screens/ReligiouslyUnaffiliatedLineVis.svelte
@@ -2,6 +2,8 @@
   import { draw } from 'svelte/transition';
   import { scaleLinear } from 'd3-scale';
   import NavigationArrow from '../components/NavigationArrow.svelte';
+  import DefinitionBubble from '../components/MiniDefinitionBubble.svelte';
+  import FunFactBubble from '../components/FunFactBubble.svelte';
 
   export let currIndex;
   export let index;
@@ -26,11 +28,18 @@
     ["Agnostic", ""],
     ["Atheist", ""],
   ];
+
+  let tooltipText = [
+    ["A person who believes in", "Jesus Christ and follows his", "teachings"],
+    [""],
+    ["A person who believes", "that nothing is known or", "can be known of the", "existence or nature of God"],
+    ["A person who doesn't", "believe in the existence", "of God(s)"]
+  ];
 </script>
 
 <div class="page" id={"page_"+index}>
   <h1>Belief in heaven and hell is less common among religiously unaffiliated Americans</h1>
-    <svg width=800 height=450>
+    <svg width=800 height=500>
       <text id="topTooltip" x=20 y=10>% who say they believe in</text>
       <text id="topTooltip" x=20 y=25>heaven and hell...</text>
 
@@ -41,18 +50,20 @@
         <text id="bold" x={200*(i+1)-120} y={yScale(percentList[i][0])-55}>{percentList[i][0] + "%"}</text>
         <text id="bold" x={200*(i+1)+50-120} y={yScale(percentList[i][1])-55}>{percentList[i][1] + "%"}</text>
         <!-- TODO change image href -->
-        <image x={200*(i+1)-120-imageSize/2} y={yScale(percentList[i][0])-55} width={imageSize} height={imageSize} href="/images/hell.svg"/>
+        <image x={200*(i+1)-120-imageSize/2} y={yScale(percentList[i][0])-55} width={imageSize} height={imageSize} href="/images/heaven.svg"/>
         <image x={200*(i+1)+50-120-imageSize/2} y={yScale(percentList[i][1])-55} width={imageSize} height={imageSize} href="/images/hell.svg"/>
-        <line x1={200*(i+1)-120-0.5} x2={200*(i+1)-120-0.5} y2={yScale(0)} y1={yScale(percentList[i][0])}/>
-        <line x1={200*(i+1)+50-120-0.5} x2={200*(i+1)+50-120-0.5} y2={yScale(0)} y1={yScale(percentList[i][1])}/>
+        <line id="yellow" x1={200*(i+1)-120-4} x2={200*(i+1)-120-4} y2={yScale(0)} y1={yScale(percentList[i][0])}/>
+        <line id="red" x1={200*(i+1)+50-120-0.5} x2={200*(i+1)+50-120-0.5} y2={yScale(0)} y1={yScale(percentList[i][1])}/>
 
         <text x={200*(i+1)-100} y=370>{labelList[i][0]}</text>
         <text x={200*(i+1)-100} y=385>{labelList[i][1]}</text>
+
+        {#if i != 1}
+        <DefinitionBubble position = {[200*(i+1)-100+35, 355]} tooltipOffset = {[20, 20]} text = {tooltipText[i]}/>
+        {/if}
       {/each}
 
       <line x1=50 x2=780 y1=340 y2=340 stroke="black"/>
-
-      <!-- TODO add tooltips -->
 
     </svg>
   <NavigationArrow link={"#page_"+(index+1)} isAtBottom={true} arrowType="4"/>
@@ -105,11 +116,20 @@
 
   line {
     stroke: black;
+    stroke-width: 1.5;
   }
 
   rect {
     fill: #C8AD9A;
     opacity: 0.5;
+  }
+
+  #yellow {
+    stroke: #FFE2CE;
+  }
+
+  #red {
+    stroke: #C95033;
   }
 
 </style>
