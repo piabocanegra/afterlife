2f2cff34a4aa2cf23c928bcd3efefb61afccaf8a
Lesley Huang <ningning621@gmail.com>
2022-07-12 12:59:29 -0700

    fixed fonts on fun fact page

--- src/screens/funfact/HeavenHellFunFactVis.svelte
+++ src/screens/funfact/HeavenHellFunFactVis.svelte
@@ -7,7 +7,7 @@
 
 <div class="page" id={"page_"+index}>
   <img src="/images/late_penitents_celestial_rose.jpg" alt="late penitents and celestial rose"/>
-  <h3>So what does heaven and hell look like?</h3>
+  <h1>So what does heaven and hell look like?</h1>
   <p>Let's find out!</p>
   <FunFactBubble bubbleId="1"/>
   <FunFactBubble bubbleId="2"/>
@@ -20,8 +20,9 @@
     position: relative;
   }
 
-  h3 {
+  h1 {
     max-width: 30%;
+    padding: 0.5em;
     position: absolute;
     top: 50%;
     left: 50%;
